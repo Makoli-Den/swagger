91c298f3f75492c4f615af2dcc8a4aa49a14112f
Makoli-Den <m4n4kov.d@yandex.ru>
2022-07-15 08:35:10 +0300

    updated

--- expressApp.js
+++ expressApp.js
@@ -1,11 +1,18 @@
+// Подключаем js фреймворк для запуска сайта
 const express = require('express');
+// Подключаем библиотеку CORS для кроссдоменных запросов
+// На данный момент не сильно необходимо, но лучше оставить
 const cors = require('cors');
-const swaggerApp = express();
+// Подключаем библиотеку сваггера для express фреймворка
+// Подключаем документацию для сваггера
 const swaggerUi = require('swagger-ui-express');
 const openApiDocumentation = require('./openApi.json');
-
+// Создаем экземпляр приложения
+const swaggerApp = express();
+// Включаем CORS, и включаем с сваггер с параметром withCredentials:true для куков, используя документацию
 swaggerApp.use(cors());
 swaggerApp.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocumentation, { swaggerOptions: { withCredentials: true } }));
+// Запускаем сайт под нужным нам портом, порт может быть любой не занятый во внутренней сети
 swaggerApp.listen(3333, () => {
-    console.log(`Application listening on port 3333!`);
+    console.log(`Swagger-ui for creatio is being listened on port 3333!`);
 });
